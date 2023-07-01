2a5ed706c4e754b221025b7a688ec4c6406bd80e
Надыр <131809409+NadyrStolyarov@users.noreply.github.com>
2023-07-01 20:28:07 +0300

    Add files via upload

---
+++ KR/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите количество элементов массива:");
+int num = Convert.ToInt32(Console.ReadLine());
+string [] stringArr = new string [num];
+
+void array(string [] stringArr)
+{
+    for (int i = 0;i<stringArr.Length;i++)
+    {
+        Console.Write($"Введите {i+1} элемент массива: ");
+        stringArr[i] = Console.ReadLine();
+    }
+}
+
+string [] ArrayResault(string [] stringArr)
+{
+    int c = 0;
+    for (int i = 0;i<stringArr.Length;i++)
+    {
+        if(stringArr[i].Length <=3)
+        c++;
+    }
+    string [] ArrRes = new string [c];
+    int j = 0;
+    for (int i = 0;i<stringArr.Length;i++)
+    {
+        if(stringArr[i].Length <=3)
+        {
+            ArrRes[j] = stringArr[i];
+            j++;
+        }
+    }
+    return ArrRes;
+}
+
+void printA(string [] stringArr)
+{
+    Console.Write("[");
+    for (int i = 0;i<stringArr.Length;i++)
+    {
+    Console.Write($"{stringArr[i]}, ");
+    }
+    Console.Write("]");
+}
+
+
+array(stringArr);
+Console.WriteLine("массив из строк, длина которых <= 3 символам:");
+printA(ArrayResault(stringArr));
